47daa09c369e70984c6e11771e34b068c5a8ff0a
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-09-29 16:17:36 +0300

    Added: read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,44 @@
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+import {dirExist} from "./copy.js";
+import fs, {promises} from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const targetFile = join(__dirname, 'files', 'fileToRead.txt');
+
+    const existFile = await dirExist(targetFile);
+    if (!existFile) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        const file = await readFile(targetFile);
+        console.log(file)
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 await read();
+
+function readFile(pathToFile) {
+    return new Promise((resolve, reject) => {
+        const data = [];
+        const readStream = fs.createReadStream(pathToFile, 'utf8');
+
+        readStream.on('data', (chunk) => {
+            data.push(chunk);
+        });
+
+        readStream.on('end', () => {
+            resolve(data.join(''));
+        });
+
+        readStream.on('error', (error) => {
+            reject(error);
+        });
+    });
+}
