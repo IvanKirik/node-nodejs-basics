6647461a966c9ad0b71e18d0d89a1948d4c97855
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-09-29 14:46:23 +0300

    Added: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+import { fileURLToPath } from 'url';
+import { promises } from 'fs';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const folderPath = join(__dirname, 'files');
+    const filePath = join(folderPath, 'fresh.txt');
+
+    try {
+        await promises.access(filePath);
+        console.error('Error: FS operation failed');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            try {
+                await promises.mkdir(folderPath, { recursive: true });
+                await promises.writeFile(filePath, 'I am fresh and young', 'utf8');
+                console.log('File created successfully');
+            } catch (writeErr) {
+                console.error('Error writing file or creating directory', writeErr);
+            }
+        } else {
+            console.error('Error accessing the file', err);
+        }
+    }
 };
 
 await create();
