268c95387f6c258987f36626046c93d49696a570
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-09-29 17:46:19 +0300

    Added: transformStream transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from 'stream';
+
+
+class ReverseTransform extends Transform {
+    _transform(chunk, encoding, callback) {
+        const reversed = chunk.toString().split('').reverse().join('');
+        this.push(reversed);
+        callback();
+    }
+}
+
 const transform = async () => {
-    // Write your code here 
+    const reverseStream = new ReverseTransform();
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
+    console.log('Enter you text:');
 };
 
 await transform();
