c2fcddbb5d4fe142dfed08edd357f5d2cdd4aac5
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-09-29 16:30:35 +0300

    Added: env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const env = process.env;
+    const rssVariables = Object.keys(env)
+        .filter(key => key.startsWith('RSS_'))
+        .map(key => `${key}=${env[key]}`);
+
+    if (rssVariables.length > 0) {
+        console.log(rssVariables.join('; '));
+    } else {
+        console.log('No RSS_ variables found');
+    }
 };
 
 parseEnv();
