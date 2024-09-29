f3386e522670e1044d4fab5625cc3f5bf4d48a88
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-09-29 17:29:54 +0300

    Added: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,29 @@
+import { createHash } from 'crypto';
+import {createReadStream} from "fs";
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt')
+
+    const hash = createHash('sha256');
+
+    const readStream = createReadStream(filePath);
+
+    readStream.on('data', (chunk) => {
+        hash.update(chunk);
+    });
+
+    readStream.on('end', () => {
+        console.log(`SHA256 hash: ${hash.digest('hex')}`);
+    });
+
+    readStream.on('error', (err) => {
+        console.error('Error reading file:', err);
+    });
 };
 
 await calculateHash();
