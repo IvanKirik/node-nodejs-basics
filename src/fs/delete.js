9bbae7a527a38bd414daacbee79ade64f1a9c59f
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-09-29 16:13:17 +0300

    Added: list.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
-const remove = async () => {
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+import {dirExist} from "./copy.js";
+import {promises} from "fs";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const remove = async () => {
+    const targetFile = join(__dirname, 'files', 'fileToRemove.txt');
+
+    const existFile = await dirExist(targetFile);
+    if (!existFile) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        await promises.rm(targetFile)
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 await remove();
--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,25 @@
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+import {dirExist} from "./copy.js";
+import fs, {promises} from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const targetDir = join(__dirname, 'files');
+
+    const existDir = await dirExist(targetDir);
+    if (!existDir) {
+        throw new Error('FS operation failed');
+    }
+
+    try {
+        const files = await fs.promises.readdir(targetDir);
+        console.log(files.join(', '))
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 await list();
