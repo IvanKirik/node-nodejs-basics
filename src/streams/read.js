46fc0cfaf2cf959344c8662f8f81aedae59f61f0
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-09-29 17:33:59 +0300

    Added: read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,27 @@
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+import {createReadStream} from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const pathToFile = join(__dirname, 'files', 'fileToRead.txt')
+
+    const data = [];
+    const readStream = createReadStream(pathToFile, 'utf8');
+
+    readStream.on('data', (chunk) => {
+        data.push(chunk);
+    });
+
+    readStream.on('end', () => {
+        console.log(data.join(''));
+    });
+
+    readStream.on('error', (error) => {
+        console.log(error);
+    });
 };
 
 await read();
