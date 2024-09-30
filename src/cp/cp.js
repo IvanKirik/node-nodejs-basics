ed6205413375ca1d258cf22ad4960e2a61bdea6b
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-09-30 15:05:21 +0300

    Added: cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,27 @@
+import { spawn } from 'child_process';
+import {fileURLToPath} from "url";
+import {dirname, join} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const childProcess = spawn('node', [join(__dirname, 'files', 'script.js'), ...args]);
+
+    process.stdin.pipe(childProcess.stdin);
+
+    childProcess.stdout.pipe(process.stdout);
+
+    childProcess.stderr.on('data', (data) => {
+        console.error(`Ошибка в дочернем процессе: ${data}`);
+    });
+
+    childProcess.on('close', (code) => {
+        console.log(`Дочерний процесс завершен с кодом ${code}`);
+    });
 };
 
+const args = process.argv.slice(2);
+
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(args);
